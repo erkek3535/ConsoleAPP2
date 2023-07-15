89a5a45416b6f001e770c8f02e4d443b64abf9e2
erkek3535 <alimuraterkek35@gmail.com>
2023-07-15 12:04:20 +0300

    ConsoleAPP2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yil;
+            Console.WriteLine("Doğum yılınızı girin:");
+            yil=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Şu an {0} yaşındasınız",DateTime.Now.Year - yil);
+            Console.ReadKey();
+        
+        }
+    }
+}
